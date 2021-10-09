f96802232057de80820e4a696109b1777c5a1223
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-10-09 03:24:06 -0300

    inplementação header
    Componente de cabeçario.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Routes from './routes/Routes'
+import Header from './components/header/Header';
 
 function App() {
   
   return (
     <div className="App">
+      <Header/>
       <Routes/>
     </div>
   );
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Biblioteca de Jogos</h1>
+    </div>
+  )
+}
