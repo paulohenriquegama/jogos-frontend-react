749caf07cde2dc03bca3216e9a2769caf873360c
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-10-09 03:26:13 -0300

    Implementação de funcionalidade
    Implementada a função para direcionar para viewGame.

--- src/components/cardList/gameCard/GameCard.jsx
+++ src/components/cardList/gameCard/GameCard.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useHistory } from 'react-router';
 import "./gameCard.css"
 
 export const GameCard = ({game}) => {
+  const history = useHistory();
   
+  const handleClick = () => {
+    history.push(`/game/view/${game.id}`)
+  }
 
   return (
-    <div className="gameCard">
+    <div className="gameCard" onClick={handleClick}>
       <div>
         {game.title}
       </div>
