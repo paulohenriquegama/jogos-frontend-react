ef8099516ed14e10efd8bcd68bec776e9812f9d0
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-10-23 01:06:29 -0300

    update admin in createUser

--- src/pages/updateUser/UpdateUser.js
+++ src/pages/updateUser/UpdateUser.js
@@ -1,8 +1,26 @@
 import { Api } from '../../api/Api'
+import React, { useEffect, useState } from 'react'
 
-import './createUser.css'
+import './updateUser.css'
 
-export default function CreateGame(props) {
+export default function UpdateUser(props) {
+  const id = props.match.params.id;
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+    const loadUser = async () => {
+        const response = await Api.buildApiGetRequest(
+            Api.readByIdUserUrl(id),
+            true
+        );
+
+        const results = await response.json();
+
+        setUser(results);
+    };
+
+    loadUser();
+  },[id]); 
 
   const handleSubmit = async e => {
     e.preventDefault()
@@ -11,7 +29,6 @@ export default function CreateGame(props) {
     const email = e.target.email.value
     const password = e.target.password.value
     const cpf = e.target.cpf.value
-    const admin = false
 
     const playload = {
       name,
@@ -19,17 +36,16 @@ export default function CreateGame(props) {
       email,
       password,
       cpf,
-      admin,
     }
-    const response = await Api.buildApiPostRequest(
-      Api.createUserUrl(),
+    const response = await Api.buildApiPatchRequest(
+      Api.updateUserUrl(id),
       playload,
       true
     )
 
     const body = await response.json()
 
-    if (response.status === 201) {
+    if (response.status === 200) {
       const id = body.id
       props.history.push(`/user/${id}`)
     }
@@ -43,19 +59,19 @@ export default function CreateGame(props) {
           <h2>Cadastro de Usuario</h2>
         </div>
         <label htmlFor="name">Nome:</label>
-        <input type="text" id="name" name="name" required />
+        <input type="text" id="name" name="name" defaultValue={user.name} required />
 
         <label htmlFor="surname">Sobrenome:</label>
-        <input type="text" id="surname" name="surname" />
+        <input type="text" id="surname" name="surname" defaultValue={user.surname} />
 
         <label htmlFor="email">E-mail:</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" defaultValue={user.email} required />
 
         <label htmlFor="password">Senha:</label>
-        <input type="password" id="password" name="password" required />
+        <input type="password" id="password" name="password" defaultValue={user.password} required />
 
         <label htmlFor="cpf">Cpf:</label>
-        <input type="text" id="cpf" name="cpf" />
+        <input type="text" id="cpf" name="cpf" defaultValue={user.cpf}/>
 
         <button type="submit" class="btn-pattern">
           Enviar
